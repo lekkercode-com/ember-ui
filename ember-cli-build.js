d6a54108e047877fc77ad75834f2b77e776c6707
Vasyl Marchuk <public@vasylmarchuk.com>
2022-07-21 17:07:33 +0300

    Reinstall ember-cli-sass package, remove node-sass, update package-lock.json format
    The project would not build on the latest LTS version of node.js (16.16) and I had to rebuild package-lock.json, update ember-cli-sass, and remove node-sass (the source of build errors)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,12 +1,10 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const nodeSass = require('node-sass');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     sassOptions: {
-      implementation: nodeSass,
       includePaths: ['node_modules/bulma'],
     },
   });
