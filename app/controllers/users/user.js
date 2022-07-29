d387831b9f650eb46eb31ceb340a59413c895276
Vasyl Marchuk <public@vasylmarchuk.com>
2022-07-29 17:05:57 +0300

    Use UserArchivedStatus Component in the User template

--- app/controllers/users/user.js
+++
@@ -1,9 +0,0 @@
-import Controller from '@ember/controller';
-import { action } from '@ember/object';
-
-export default class UsersUserController extends Controller {
-  @action async toggleArchived (user) {
-    user.archived = !user.archived;
-    user.save();
-  }
-}
